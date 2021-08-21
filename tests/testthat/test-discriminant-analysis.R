cc1cabd0c3fbe02bd485b798d025c170427dc7d6
Youngrok Lee <cakeil@gmail.com>
2021-08-21 15:07:50 -0400

    테스트 추가 - LDA 스코어

--- tests/testthat/test-discriminant-analysis.R
+++ tests/testthat/test-discriminant-analysis.R
@@ -16,3 +16,31 @@ test_that("Discriminant function matches", {
     failure_message = "Estimated discriminant function does not match to book example"
   )
 })
+
+test_that("LDA score matches", {
+  expect(
+    all(
+      dplyr::near(
+        ld_fun(binaryclass2, class, c(x1, x2)) %>%
+          score_da(binaryclass2, c(x1, x2)) %>%
+          as.matrix(),
+        matrix(
+          data = c(
+            9.2051470,	6.971538,
+            -1.9363321,	0.489223,
+            11.0057900,	10.246458,
+            4.5909990,	8.423307,
+            7.4045039,	3.696619,
+            5.0411598,	1.367036,
+            5.7164010,	6.532631,
+            4.0282981,	9.368644,
+            0.4270119,	2.818805
+          ),
+          nrow = 9, ncol = 2, byrow = TRUE
+        ),
+        tol = 1e-6
+      )
+    ),
+    failure_message = "Estimated discriminant scores do not match to book example"
+  )
+})
