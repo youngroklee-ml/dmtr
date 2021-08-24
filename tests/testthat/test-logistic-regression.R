cebccfe2dfa0e1544b3b210e9c92ffa15a3578e2
Youngrok Lee <cakeil@gmail.com>
2021-08-23 22:53:29 -0400

    수정 - 테스트 tolerance 변경

--- tests/testthat/test-logistic-regression.R
+++ tests/testthat/test-logistic-regression.R
@@ -4,6 +4,6 @@ test_that("binary logistic regression coefficient", {
   expect_equal(
     fit_binary_logistic_regression(student, y, x1:x3, .reflevel = "보통")[["betas"]],
     c("(Intercept)" = -30.51, x1 = 2.031, x2 = 3.471, x3 = 2.414),
-    tolerance = 0.01
+    tolerance = 1e-3
   )
 })
