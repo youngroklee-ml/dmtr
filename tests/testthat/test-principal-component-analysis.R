31a8ed729b99277271de60ca22a9d43905a44cad
Youngrok Lee <cakeil@gmail.com>
2021-08-29 18:55:02 -0400

    테스트 추가 - 주성분 회귀분석; 교재 예제 답 오류

--- tests/testthat/test-principal-component-analysis.R
+++ tests/testthat/test-principal-component-analysis.R
@@ -8,8 +8,18 @@ test_that("principal component eigen values matches to book example", {
 
 test_that("principal component eigen values matches to book example", {
   expect_equal(
-    fit_pca(pcrdata, x1:x3, .center = FALSE, .scale = FALSE)[["eig"]] %>% unname(),
+    fit_pca(pcrdata, x1:x3, .scale = FALSE)[["eig"]] %>% unname(),
     c(54.709, 2.769, 0.122),
     tolerance = 1e-3
   )
 })
+
+test_that("principal component regression matches to book example", {
+  expect_equal(
+    fit_pcr(pcrdata, y, x1:x3, .scale = FALSE, .pc = 2L)[["org_betas"]][c("x1", "x2", "x3")],
+    c(x1 = 2.1303, x2 = 2.7217, x3 = -1.7379),
+    tolerance = 1e-3
+  )
+})
+
+
