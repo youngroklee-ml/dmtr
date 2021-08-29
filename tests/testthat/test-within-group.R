e99e687e470d64012db914f35849c49f12f35f9c
Youngrok Lee <cakeil@gmail.com>
2021-08-29 14:19:55 -0400

    테스트 추가 - 합동 분산-공분산행렬

--- tests/testthat/test-within-group.R
+++ tests/testthat/test-within-group.R
@@ -1,3 +1,13 @@
+test_that("pooled variance-covariance matrix matches to book example", {
+  expect_equal(
+    pooled_variance(binaryclass2, class, c(x1, x2)) %>%
+      as.numeric(),
+    c(3.8857, 2.1143, 2.1143, 2.4571),
+    tolerance = 1e-3
+  )
+})
+
+
 test_that("group_mean returns correct length of list", {
   expect_equal(
     length(group_mean(binaryclass2, class, x1)),
