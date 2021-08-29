be6313e77d4f3d484e3da72ee95962958c0fb311
Youngrok Lee <cakeil@gmail.com>
2021-08-29 18:46:28 -0400

    테스트 추가 - 주성분분석 고유치; unscaled 경우

--- tests/testthat/test-principal-component-analysis.R
+++ tests/testthat/test-principal-component-analysis.R
@@ -1,10 +1,15 @@
-data(financials, package = "dmtr")
-fit <- fit_pca(financials, roa:turnover)
+test_that("principal component eigen values matches to book example", {
+  expect_equal(
+    fit_pca(financials, roa:turnover)[["eig"]] %>% unname(),
+    c(2.7615, 1.6057, 0.5506, 0.0641, 0.0183),
+    tolerance = 1e-3
+  )
+})
 
 test_that("principal component eigen values matches to book example", {
   expect_equal(
-    fit[["eig"]] %>% unname(),
-    c(2.7615, 1.6057, 0.5506, 0.0641, 0.0183),
+    fit_pca(pcrdata, x1:x3, .center = FALSE, .scale = FALSE)[["eig"]] %>% unname(),
+    c(54.709, 2.769, 0.122),
     tolerance = 1e-3
   )
 })
